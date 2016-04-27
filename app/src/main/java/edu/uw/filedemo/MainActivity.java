9ad5daef69e73f7c166cda3e68d47052b407bc7d
ying <whydang@uw.edu>
2016-04-27 12:16:15 -0700

    inprogress

--- app/src/main/java/edu/uw/filedemo/MainActivity.java
+++ app/src/main/java/edu/uw/filedemo/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.uw.filedemo;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -11,6 +13,14 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,15 +36,55 @@ public class MainActivity extends AppCompatActivity {
         externalButton = (RadioButton)findViewById(R.id.radio_external);
     }
 
+    // test to see if the storage stage is mounted such that we can write inside it
+    // returns T/F
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (state.equals(Environment.MEDIA_MOUNTED)) {
+            return true;
+        }
+        return false;
+    }
+
 
     public void saveFile(View v){
         Log.v(TAG, "Saving file...");
         EditText textEntry = (EditText)findViewById(R.id.textEntry); //what we're going to save
 
         if(externalButton.isChecked()){ //external storage
+            if (isExternalStorageWritable()) {
+                //File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+                File dir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+                File file = new File(dir, "notes.txt");
 
+                try {
+                    PrintWriter out = new PrintWriter(file);
+                    out.println(textEntry.getText().toString());
+                    out.close();
+                } catch (IOException e) {
+                    Log.v(TAG, Log.getStackTraceString(e));
+                }
+            }
         }
         else { //internal storage
+//            File dir = getFilesDir();
+//            File file = new File(dir, "notes.txt");
+//
+//            try {
+//                PrintWriter out = new PrintWriter(file);
+//                out.println(textEntry.getText().toString());
+//                out.close();
+//            } catch (IOException e) {
+//                Log.v(TAG, Log.getStackTraceString(e));
+//            }
+
+            // gives a files outputstream
+            try {
+                FileOutputStream fos = openFileOutput("noes.txt", MODE_PRIVATE); // takes in integer constant representing MODE
+
+            } catch (IOException e) {
+
+            }
 
         }
     }
@@ -46,7 +96,26 @@ public class MainActivity extends AppCompatActivity {
         textDisplay.setText(""); //clear initially
 
         if(externalButton.isChecked()){ //external storage
+            if (isExternalStorageWritable()) {
+                //File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
+                File dir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+                File file = new File(dir, "notes.txt");
 
+                try {
+                    BufferedReader reader = new BufferedReader(new FileReader(file));
+                    StringBuilder builder = new StringBuilder();
+
+                    String line = reader.readLine();
+                    while (line != null) {
+                        builder.append(line + "\n");
+                        line = reader.readLine();
+
+                    }
+
+                } catch (IOException e) {
+                    Log.v(TAG, Log.getStackTraceString(e));
+                }
+            }
         }
         else { //internal storage
 
@@ -56,13 +125,27 @@ public class MainActivity extends AppCompatActivity {
 
     public void shareFile(View v) {
         Log.v(TAG, "Sharing file...");
-
+        Uri fileUri = null;
         if(externalButton.isChecked()){ //external storage
-
+            File dir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+            File file = new File(dir, "notes.txt");
+            fileUri = Uri.fromFile(file);
         }
         else { //internal storage
 
         }
+        // send to share, lets other application handle it
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_STREAM, fileUri);
+
+        // sets the chooser and a label for when it pops up
+        // only happens in the case there are 2+ that listens to the intent
+        Intent chooser = Intent.createChooser(intent, "Share File");
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(chooser);
+        }
+
     }
 
 
--- app/src/main/java/edu/uw/filedemo/PhotoActivity.java
+++ app/src/main/java/edu/uw/filedemo/PhotoActivity.java
@@ -2,12 +2,19 @@ package edu.uw.filedemo;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class PhotoActivity extends AppCompatActivity {
 
@@ -24,10 +31,27 @@ public class PhotoActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    private Uri picUri;
+
     public void takePicture(View v){
         Log.v(TAG, "Taking picture...");
 
+        File img = null;
+        try {
+            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmSS").format(new Date());
+            File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            img = new File(dir, "PIC_" + timestamp);
+            img.createNewFile();
+            picUri = Uri.fromFile(img);
+
+        } catch (IOException e) {
+            Log.v(TAG, Log.getStackTraceString(e));
+        }
+
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // mediastore extra_output tells us ... to take a picture and put it in picUri
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, picUri);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
@@ -37,10 +61,13 @@ public class PhotoActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
 
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap)extras.get("data");
+
+
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap)extras.get("data");
             ImageView imageView = (ImageView)findViewById(R.id.imgThumbnail);
-            imageView.setImageBitmap(imageBitmap);
+            imageView.setImageURI(picUri);
+//            imageView.setImageBitmap(imageBitmap);
 
         }
         super.onActivityResult(requestCode, resultCode, data);
